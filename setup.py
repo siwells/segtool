4f086b7f3afffc848bd50e2aceb3c7b809a01767
Simon Wells <siwells@gmail.com>
2014-08-13 13:27:48 +0100

    setup.py so we can easily distribute the package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='segtool',
+  version='0.1.0',
+  description='Segmenation Analysis Tool',
+  long_description='Performs the calculations required to perform an Annable-style segmentation analaysis of travellers attitudes towards transport modes.',
+  license='GNU GPL Version 3',
+  author='Simon Wells',
+  author_email='siwells@gmail.com',
+  url = 'https://github.com/siwells/segtool',
+  packages=['segtool'],)
+
